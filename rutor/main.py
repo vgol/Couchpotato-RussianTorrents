80defeccb8d67d5be810dba094f8566a090e1480
Vladimir Golubkov <vgol.mail@gmail.com>
2018-03-17 22:27:48 +0300

    Additional logging for html.

--- rutor/main.py
+++ rutor/main.py
@@ -1,37 +1,34 @@
-# -*- coding: utf8 -*-
+import re
 import traceback
-
 from bs4 import BeautifulSoup
-from couchpotato.core.helpers.encoding import tryUrlencode
+
 from couchpotato.core.helpers.variable import tryInt
 from couchpotato.core.logger import CPLog
 from couchpotato.core.media._base.providers.torrent.base import TorrentProvider
 from couchpotato.core.media.movie.providers.base import MovieProvider
 
-import re
-
 
 log = CPLog(__name__)
 
 
-class rutor(TorrentProvider, MovieProvider):
+class Rutor(TorrentProvider, MovieProvider):
 
     baseurl = 'http://rutor.is/'
     urls = {
-        'test' : 'http://rutor.is',
-        'detail' : baseurl + 'torrent/%s',
-        'search' : baseurl + 'search/0/0/100/8/%s',
-        'download' : baseurl + 'download/%s',
+        'test': 'http://rutor.is',
+        'detail': baseurl + 'torrent/%s',
+        'search': baseurl + 'search/0/0/100/8/%s',
+        'download': baseurl + 'download/%s',
     }
 
-    http_time_between_calls = 1 #seconds
+    http_time_between_calls = 1  # seconds
     cat_backup_id = None
 
     def _searchOnTitle(self, title, movie, quality, results):
 
-        log.debug('Searching rutor for %s' % (title))
+        log.debug('Searching rutor for %s' % title)
 
-        url = self.urls['search'] % (title.replace(':', ' '))
+        url = self.urls['search'] % title.replace(':', ' ')
         data = self.getHTMLData(url)
 
         log.debug('Received data from rutor')
@@ -41,61 +38,64 @@ class rutor(TorrentProvider, MovieProvider):
             html = BeautifulSoup(data)
 
             try:
-		result_table = html.find('div', attrs = {'id' : 'index'})
+                result_table = html.find('div', attrs={'id': 'index'})
+                if not result_table:
+                    log.debug("<div id=index> not found in html:")
+                    log.debug(html)
                 result_table = result_table.find('table')
                 if not result_table:
                     log.debug('No table results from rutor')
                     return
 
-                torrents = result_table.find_all('tr', attrs = {'class' : ['gai','tum']})
+                torrents = result_table.find_all('tr', attrs={'class': ['gai', 'tum']})
                 for result in torrents:
                     all_cells = result.find_all('td')
-		    dl_cell = all_cells[1].find_all('a')[0]
-		    title_cell = all_cells[1].find_all('a')[2]
+                    dl_cell = all_cells[1].find_all('a')[0]
+                    title_cell = all_cells[1].find_all('a')[2]
                     # log.debug('Total rows in a line of result table:' + str(len(all_cells)))
-		    if (len(all_cells) == 5):
-		    	cell_num = 3
-		    else:
-			cell_num = 2
-		    size_cell = all_cells[cell_num]
-                    seed_cell = all_cells[cell_num+1].find('span', attrs = {'class' : 'green'})
-		    if seed_cell:
-			seed_cell = seed_cell.getText()
-                    leech_cell = all_cells[cell_num+1].find('span', attrs = {'class' : 'red'})
-		    if leech_cell:
-			leech_cell = leech_cell.getText()		    
+                    if len(all_cells) == 5:
+                        cell_num = 3
+                    else:
+                        cell_num = 2
+                    size_cell = all_cells[cell_num]
+                    seed_cell = all_cells[cell_num + 1].find('span', attrs={'class': 'green'})
+                    if seed_cell:
+                        seed_cell = seed_cell.getText()
+                    leech_cell = all_cells[cell_num + 1].find('span', attrs={'class': 'red'})
+                    if leech_cell:
+                        leech_cell = leech_cell.getText()
 
-		    seed_cell = seed_cell.replace(u'&nbsp;', '')
-		    leech_cell = leech_cell.replace(u'&nbsp;','')
+                    seed_cell = seed_cell.replace(u'&nbsp;', '')
+                    leech_cell = leech_cell.replace(u'&nbsp;', '')
 
-		    size_cell = size_cell.getText()
+                    size_cell = size_cell.getText()
                     topic_id = dl_cell['href']
                     topic_id = topic_id.replace('/download/', '')
-                    
-                    size = size_cell.replace(u'&nbsp;GB','')
-		
 
-                    # Workaround for filtering 1080p and 720p by CouchPotato: BDRip is a source not a video quality!
+                    size = size_cell.replace(u'&nbsp;GB', '')
+
+                    # Workaround for filtering 1080p and 720p by CouchPotato:
+                    # BDRip is a source not a video quality!
                     title = title_cell.getText().replace('BDRip', '')
                     title = re.sub('^.*? / ', '', title)
-                    title = re.sub('[\[\]\(\)/ ]', '.', title)
+                    title = re.sub('[\[\]()/ ]', '.', title)
                     title = re.sub(':', '-', title)
                     title = re.sub('\.\.+', '.', title)
 
                     torrent_name = title
-                    torrent_size = self.parseSize( size )
+                    torrent_size = self.parseSize(size)
                     torrent_seeders = tryInt(seed_cell)
                     torrent_leechers = tryInt(leech_cell)
                     torrent_detail_url = self.urls['detail'] % topic_id
                     torrent_url = self.urls['download'] % topic_id
 
-                    log.debug('Title %s' % (torrent_name))
-                    log.debug('Size %s' % (torrent_size))   
-                    log.debug('Forum %s' % (torrent_detail_url))
-                    log.debug('Dl %s' % (torrent_url))
-                    log.debug('seed %d' % (torrent_seeders))
-                    log.debug('leech %d' % (torrent_leechers))
-                    
+                    log.debug('Title %s' % torrent_name)
+                    log.debug('Size %s' % torrent_size)
+                    log.debug('Forum %s' % torrent_detail_url)
+                    log.debug('Dl %s' % torrent_url)
+                    log.debug('seed %d' % torrent_seeders)
+                    log.debug('leech %d' % torrent_leechers)
+
                     results.append({
                         'id': topic_id,
                         'name': torrent_name,
@@ -105,7 +105,6 @@ class rutor(TorrentProvider, MovieProvider):
                         'url': torrent_url,
                         'detail_url': torrent_detail_url,
                     })
-                    
 
-            except:
+            except Exception:
                 log.error('Failed to parse rutor: %s' % (traceback.format_exc()))
